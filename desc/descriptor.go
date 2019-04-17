864101fba6f19706a127f96308322c6e9352c0ee
Richard Artoul <rartoul@uber.com>
2019-04-17 14:53:59 -0400

    [PERF] - Dont allocate string unnecessarily

--- desc/descriptor.go
+++ desc/descriptor.go
@@ -943,6 +943,11 @@ func (fd *FieldDescriptor) GetType() dpb.FieldDescriptorProto_Type {
 	return fd.proto.GetType()
 }
 
+// GetTypeString returns a lower-cased string that represents the type of this field.
+func (fd *FieldDescriptor) GetTypeString() string {
+	return strings.ToLower(fd.proto.GetType().String())
+}
+
 // GetLabel returns the label for this field. The label can be required (proto2-only),
 // optional (default for proto3), or required.
 func (fd *FieldDescriptor) GetLabel() dpb.FieldDescriptorProto_Label {
