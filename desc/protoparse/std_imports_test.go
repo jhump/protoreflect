76687a979bacab2718c4e95eda6137fe1062afdc
Joshua Humphries <joshua@bluegosling.com>
2019-07-17 22:38:31 -0400

    clone standard imports to avoid sharing (#238)

--- desc/protoparse/std_imports_test.go
+++ desc/protoparse/std_imports_test.go
@@ -3,16 +3,18 @@ package protoparse
 import (
 	"testing"
 
+	"github.com/golang/protobuf/proto"
+
 	"github.com/jhump/protoreflect/internal/testutil"
 )
 
 func TestStdImports(t *testing.T) {
 	// make sure we can successfully parse all standard imports
 	var p Parser
-	for name, proto := range standardImports {
+	for name, fileProto := range standardImports {
 		fds, err := p.ParseFiles(name)
 		testutil.Ok(t, err)
 		testutil.Eq(t, 1, len(fds))
-		testutil.Eq(t, proto, fds[0].AsFileDescriptorProto())
+		testutil.Require(t, proto.Equal(fileProto, fds[0].AsFileDescriptorProto()))
 	}
 }
