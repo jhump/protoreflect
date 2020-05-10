bd6223c214b0ba15d79821fc5aadad58bf67582f
Josh Humphries <jh@fullstory.com>
2020-05-10 12:15:09 -0400

    use reflection to interact with APIv2 messages instead of unmarshaling

--- dynamic/dynamic_message.go
+++ dynamic/dynamic_message.go
@@ -2328,21 +2328,7 @@ func (m *Message) mergeInto(pm proto.Message, deterministic bool) error {
 			}
 		}
 
-		u := target.FieldByName("XXX_unrecognized")
-		if u.IsValid() && u.Type() == typeOfBytes {
-			// Just store the bytes in the unrecognized field
-			ub := u.Interface().([]byte)
-			ub = append(ub, b.Bytes()...)
-			u.Set(reflect.ValueOf(ub))
-		} else {
-			// This message does not have an XXX_unrecognized in which to store
-			// this data. It could be an API v2 message, which stores unrecognized
-			// fields differently. So, in case that is what this is, let's try to
-			// unmarshal the data into it
-			if err := proto.UnmarshalMerge(b.Bytes(), pm); err != nil {
-				return err
-			}
-		}
+		setUnrecognized(target, b.Bytes())
 	}
 
 	// finally, convey unknown fields into the given message by letting it unmarshal them
@@ -2355,6 +2341,63 @@ func (m *Message) mergeInto(pm proto.Message, deterministic bool) error {
 	}
 
 	return nil
+}
+
+func setUnrecognized(val reflect.Value, data []byte) {
+	u := val.FieldByName("XXX_unrecognized")
+	if u.IsValid() && u.Type() == typeOfBytes {
+		// Just store the bytes in the unrecognized field
+		ub := u.Interface().([]byte)
+		ub = append(ub, data...)
+		u.Set(reflect.ValueOf(ub))
+		return
+	}
+
+	get, set, argType, ok := unrecognizedGetSetMethods(val)
+	if !ok {
+		return
+	}
+
+	existing := get.Call([]reflect.Value(nil))[0].Convert(typeOfBytes).Interface().([]byte)
+	if len(existing) > 0 {
+		data = append(existing, data...)
+	}
+	set.Call([]reflect.Value{reflect.ValueOf(data).Convert(argType)})
+}
+
+func unrecognizedGetSetMethods(val reflect.Value) (get reflect.Value, set reflect.Value, argType reflect.Type, ok bool) {
+	// val could be an APIv2 message. We use reflection to interact with
+	// this message so that we don't have a hard dependency on the new
+	// version of the protobuf package.
+	refMethod := val.MethodByName("ProtoReflect")
+	if !refMethod.IsValid() {
+		if val.CanAddr() {
+			refMethod = val.Addr().MethodByName("ProtoReflect")
+		}
+		if !refMethod.IsValid() {
+			return
+		}
+	}
+	refType := refMethod.Type()
+	if refType.NumIn() != 0 || refType.NumOut() != 1 {
+		return
+	}
+	ref := refMethod.Call([]reflect.Value(nil))
+	getMethod, setMethod := ref[0].MethodByName("GetUnknown"), ref[0].MethodByName("SetUnknown")
+	if !getMethod.IsValid() || !setMethod.IsValid() {
+		return
+	}
+	getType := getMethod.Type()
+	setType := setMethod.Type()
+	if getType.NumIn() != 0 || getType.NumOut() != 1 || setType.NumIn() != 1 || setType.NumOut() != 0 {
+		return
+	}
+	arg := setType.In(0)
+	if !arg.ConvertibleTo(typeOfBytes) || getType.Out(0) != arg {
+		return
+	}
+
+	return getMethod, setMethod, arg, true
 }
 
 func canConvert(src reflect.Value, target reflect.Type) bool {
@@ -2571,13 +2614,15 @@ func (m *Message) mergeFrom(pm proto.Message) error {
 
 	// now actually perform the merge
 	for fd, v := range values {
-		mergeField(m, fd, v)
+		if err := mergeField(m, fd, v); err != nil {
+			return err
+		}
 	}
 
-	u := src.FieldByName("XXX_unrecognized")
-	if u.IsValid() && u.Type() == typeOfBytes {
+	data := getUnrecognized(src)
+	if len(data) > 0 {
 		// ignore any error returned: pulling in unknown fields is best-effort
-		_ = m.UnmarshalMerge(u.Interface().([]byte))
+		_ = m.UnmarshalMerge(data)
 	}
 
 	// lastly, also extract any unknown extensions the message may have (unknown extensions
@@ -2588,6 +2633,20 @@ func (m *Message) mergeFrom(pm proto.Message) error {
 		_ = m.UnmarshalMerge(unknownExtensions)
 	}
 	return nil
+}
+
+func getUnrecognized(val reflect.Value) []byte {
+	u := val.FieldByName("XXX_unrecognized")
+	if u.IsValid() && u.Type() == typeOfBytes {
+		return u.Interface().([]byte)
+	}
+
+	get, _, _, ok := unrecognizedGetSetMethods(val)
+	if !ok {
+		return nil
+	}
+
+	return get.Call([]reflect.Value(nil))[0].Convert(typeOfBytes).Interface().([]byte)
 }
 
 // Validate checks that all required fields are present. It returns an error if any are absent.
