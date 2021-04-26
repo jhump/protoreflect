6b9f4e5755292bfa0b3703864ae613d3d350e129
Josh Humphries <jh@fullstory.com>
2021-04-26 11:40:39 -0400

    fix typo that resulted in wrong message being removed from its parent

--- desc/builder/message.go
+++ desc/builder/message.go
@@ -482,12 +482,12 @@ func (mb *MessageBuilder) TryAddNestedMessage(nmb *MessageBuilder) error {
 	needToUnlinkFirst := mb.isPresentButNotChild(nmb)
 	if needToUnlinkFirst {
 		Unlink(nmb)
-		mb.addSymbol(nmb)
+		_ = mb.addSymbol(nmb)
 	} else {
 		if err := mb.addSymbol(nmb); err != nil {
 			return err
 		}
-		Unlink(mb)
+		Unlink(nmb)
 	}
 	nmb.setParent(mb)
 	mb.nestedMessages = append(mb.nestedMessages, nmb)
