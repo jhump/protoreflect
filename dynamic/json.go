9dff476769f4c105ebd93116973f16591e6cdb60
Josh Humphries <jh@fullstory.com>
2021-11-01 11:47:04 -0400

    fix bug in dynamic message: unmarshaling JSON into google.protobuf.Value when JSON value is an array

--- dynamic/json.go
+++ dynamic/json.go
@@ -674,8 +674,10 @@ func isWellKnownValue(fd *desc.FieldDescriptor) bool {
 }
 
 func isWellKnownListValue(fd *desc.FieldDescriptor) bool {
+	// we look for ListValue; but we also look for Value, which can be assigned a ListValue
 	return !fd.IsRepeated() && fd.GetType() == descriptor.FieldDescriptorProto_TYPE_MESSAGE &&
-		fd.GetMessageType().GetFullyQualifiedName() == "google.protobuf.ListValue"
+		(fd.GetMessageType().GetFullyQualifiedName() == "google.protobuf.ListValue" ||
+			fd.GetMessageType().GetFullyQualifiedName() == "google.protobuf.Value")
 }
 
 func unmarshalJsField(fd *desc.FieldDescriptor, r *jsReader, mf *MessageFactory, opts *jsonpb.Unmarshaler) (interface{}, error) {
