6224817bacc55de881e0ffd48a6350e01795d5a9
Joshua Humphries <jh@fullstory.com>
2022-02-09 19:13:27 -0500

    grpcdynamic, grpcreflect: use grpc.ClientConnInterface and reflection.GRPCServer interfaces (#491)

--- grpcreflect/server_test.go
+++ grpcreflect/server_test.go
@@ -37,3 +37,24 @@ func TestLoadServiceDescriptors(t *testing.T) {
 		testutil.Eq(t, c.response, md.GetOutputType().GetFullyQualifiedName())
 	}
 }
+
+func TestLoadServiceDescriptor(t *testing.T) {
+	sd, err := LoadServiceDescriptor(testprotos.TestService_ServiceDesc)
+	testutil.Ok(t, err)
+
+	cases := []struct{ method, request, response string }{
+		{"DoSomething", "testprotos.TestRequest", "jhump.protoreflect.desc.Bar"},
+		{"DoSomethingElse", "testprotos.TestMessage", "testprotos.TestResponse"},
+		{"DoSomethingAgain", "jhump.protoreflect.desc.Bar", "testprotos.AnotherTestMessage"},
+		{"DoSomethingForever", "testprotos.TestRequest", "testprotos.TestResponse"},
+	}
+
+	testutil.Eq(t, len(cases), len(sd.GetMethods()))
+
+	for i, c := range cases {
+		md := sd.GetMethods()[i]
+		testutil.Eq(t, c.method, md.GetName())
+		testutil.Eq(t, c.request, md.GetInputType().GetFullyQualifiedName())
+		testutil.Eq(t, c.response, md.GetOutputType().GetFullyQualifiedName())
+	}
+}
