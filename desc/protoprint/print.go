e429ee6b3537f3d1414fbe2e8352222b41559f96
Marukhlenko Daniil <56109270+japersik@users.noreply.github.com>
2023-10-31 20:18:01 +0300

    protoprint: fix bug in escaping double-quote characters (#580)

--- desc/protoprint/print.go
+++ desc/protoprint/print.go
@@ -2015,7 +2015,7 @@ func quotedBytes(s string) string {
 		case '\t':
 			b.WriteString("\\t")
 		case '"':
-			b.WriteString("\\")
+			b.WriteString("\\\"")
 		case '\\':
 			b.WriteString("\\\\")
 		default:
@@ -2061,7 +2061,7 @@ func quotedString(s string) string {
 		case '\t':
 			b.WriteString("\\t")
 		case '"':
-			b.WriteString("\\")
+			b.WriteString("\\\"")
 		case '\\':
 			b.WriteString("\\\\")
 		default:
