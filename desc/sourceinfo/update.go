5a279673d5345a38ecaa3c3e486ecb9ca7f87797
Josh Humphries <2035234+jhump@users.noreply.github.com>
2024-09-03 08:51:03 -0400

    deprecate the Wrap* methods; add AddSourceInfoTo* replacements that are better named and allow returning an error

--- desc/sourceinfo/update.go
+++ desc/sourceinfo/update.go
@@ -2,92 +2,177 @@ package sourceinfo
 
 import (
 	"fmt"
+
 	"google.golang.org/protobuf/reflect/protoreflect"
 	"google.golang.org/protobuf/reflect/protoregistry"
 )
 
-// For back-compat reasons, these methods are named "Wrap". But they don't wrap
-// but instead "upgrade", which means they rebuild the descriptors with source
-// code info when possible.
-
-// WrapFile wraps the given file descriptor so that it will include source
-// code info that was registered with this package if the given file was
-// processed with protoc-gen-gosrcinfo. Returns fd without wrapping if fd
-// already contains source code info.
-func WrapFile(fd protoreflect.FileDescriptor) protoreflect.FileDescriptor {
-	result, err := getFile(fd)
-	if err != nil {
-		panic(err)
-	}
-	return result
+// AddSourceInfoToFile will return a new file descriptor that is a copy
+// of fd except that it includes source code info. If the given file
+// already contains source info, was not registered from generated code,
+// or was not processed with the protoc-gen-gosrcinfo plugin, then fd
+// is returned as is, unchanged.
+func AddSourceInfoToFile(fd protoreflect.FileDescriptor) (protoreflect.FileDescriptor, error) {
+	return getFile(fd)
 }
 
-// WrapMessage wraps the given message descriptor so that it will include source
-// code info that was registered with this package if the file it is defined in
-// was processed with protoc-gen-gosrcinfo. Returns md without wrapping if md's
-// parent file already contains source code info.
-func WrapMessage(md protoreflect.MessageDescriptor) protoreflect.MessageDescriptor {
-	result, err := updateDescriptor(md)
-	if err != nil {
-		panic(err)
-	}
-	return result
+// AddSourceInfoToMessage will return a new message descriptor that is a
+// copy of md except that it includes source code info. If the file that
+// contains the given message descriptor already contains source info,
+// was not registered from generated code, or was not processed with the
+// protoc-gen-gosrcinfo plugin, then md is returned as is, unchanged.
+func AddSourceInfoToMessage(md protoreflect.MessageDescriptor) (protoreflect.MessageDescriptor, error) {
+	return updateDescriptor(md)
 }
 
-// WrapEnum wraps the given enum descriptor so that it will include source
-// code info that was registered with this package if the file it is defined in
-// was processed with protoc-gen-gosrcinfo. Returns ed without wrapping if ed's
-// parent file already contains source code info.
-func WrapEnum(ed protoreflect.EnumDescriptor) protoreflect.EnumDescriptor {
-	result, err := updateDescriptor(ed)
-	if err != nil {
-		panic(err)
-	}
-	return result
+// AddSourceInfoToEnum will return a new enum descriptor that is a copy
+// of ed except that it includes source code info. If the file that
+// contains the given enum descriptor already contains source info, was
+// not registered from generated code, or was not processed with the
+// protoc-gen-gosrcinfo plugin, then ed is returned as is, unchanged.
+func AddSourceInfoToEnum(ed protoreflect.EnumDescriptor) (protoreflect.EnumDescriptor, error) {
+	return updateDescriptor(ed)
 }
 
-// WrapService wraps the given service descriptor so that it will include source
-// code info that was registered with this package if the file it is defined in
-// was processed with protoc-gen-gosrcinfo. Returns sd without wrapping if sd's
-// parent file already contains source code info.
-func WrapService(sd protoreflect.ServiceDescriptor) protoreflect.ServiceDescriptor {
-	result, err := updateDescriptor(sd)
-	if err != nil {
-		panic(err)
-	}
-	return result
+// AddSourceInfoToService will return a new service descriptor that is
+// a copy of sd except that it includes source code info. If the file
+// that contains the given service descriptor already contains source
+// info, was not registered from generated code, or was not processed
+// with the protoc-gen-gosrcinfo plugin, then ed is returned as is,
+// unchanged.
+func AddSourceInfoToService(sd protoreflect.ServiceDescriptor) (protoreflect.ServiceDescriptor, error) {
+	return updateDescriptor(sd)
 }
 
-// WrapExtensionType wraps the given extension type so that its associated
-// descriptor will include source code info that was registered with this package
-// if the file it is defined in was processed with protoc-gen-gosrcinfo. Returns
-// xt without wrapping if the parent file of xt's descriptor already contains
-// source code info.
-func WrapExtensionType(xt protoreflect.ExtensionType) protoreflect.ExtensionType {
+// AddSourceInfoToExtensionType will return a new extension type that
+// is a copy of xt except that its associated descriptors includes
+// source code info. If the file that contains the given extension
+// already contains source info, was not registered from generated
+// code, or was not processed with the protoc-gen-gosrcinfo plugin,
+// then xt is returned as is, unchanged.
+func AddSourceInfoToExtensionType(xt protoreflect.ExtensionType) (protoreflect.ExtensionType, error) {
 	if genType, err := protoregistry.GlobalTypes.FindExtensionByName(xt.TypeDescriptor().FullName()); err != nil || genType != xt {
-		return xt
+		return xt, nil // not from generated code
 	}
 	ext, err := updateField(xt.TypeDescriptor().Descriptor())
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	return extensionType{ExtensionType: xt, extDesc: ext}
+	return extensionType{ExtensionType: xt, extDesc: ext}, nil
 }
 
-// WrapMessageType wraps the given message type so that its associated
-// descriptor will include source code info that was registered with this package
-// if the file it is defined in was processed with protoc-gen-gosrcinfo. Returns
-// mt without wrapping if the parent file of mt's descriptor already contains
-// source code info.
-func WrapMessageType(mt protoreflect.MessageType) protoreflect.MessageType {
+// AddSourceInfoToMessageType will return a new message type that
+// is a copy of mt except that its associated descriptors includes
+// source code info. If the file that contains the given message
+// already contains source info, was not registered from generated
+// code, or was not processed with the protoc-gen-gosrcinfo plugin,
+// then mt is returned as is, unchanged.
+func AddSourceInfoToMessageType(mt protoreflect.MessageType) (protoreflect.MessageType, error) {
 	if genType, err := protoregistry.GlobalTypes.FindMessageByName(mt.Descriptor().FullName()); err != nil || genType != mt {
-		return mt
+		return mt, nil // not from generated code
 	}
 	msg, err := updateDescriptor(mt.Descriptor())
 	if err != nil {
+		return nil, err
+	}
+	return messageType{MessageType: mt, msgDesc: msg}, nil
+}
+
+// WrapFile is present for backwards-compatibility reasons. It calls
+// AddSourceInfoToFile and panics if that function returns an error.
+//
+// Deprecated: Use AddSourceInfoToFile directly instead. The word "wrap" is
+// a misnomer since this method does not actually wrap the given value.
+// Though unlikely, the operation can technically fail, so the recommended
+// function allows the return of an error instead of panic'ing.
+func WrapFile(fd protoreflect.FileDescriptor) protoreflect.FileDescriptor {
+	result, err := AddSourceInfoToFile(fd)
+	if err != nil {
 		panic(err)
 	}
-	return messageType{MessageType: mt, msgDesc: msg}
+	return result
+}
+
+// WrapMessage is present for backwards-compatibility reasons. It calls
+// AddSourceInfoToMessage and panics if that function returns an error.
+//
+// Deprecated: Use AddSourceInfoToMessage directly instead. The word
+// "wrap" is a misnomer since this method does not actually wrap the
+// given value. Though unlikely, the operation can technically fail,
+// so the recommended function allows the return of an error instead
+// of panic'ing.
+func WrapMessage(md protoreflect.MessageDescriptor) protoreflect.MessageDescriptor {
+	result, err := AddSourceInfoToMessage(md)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+// WrapEnum is present for backwards-compatibility reasons. It calls
+// AddSourceInfoToEnum and panics if that function returns an error.
+//
+// Deprecated: Use AddSourceInfoToEnum directly instead. The word
+// "wrap" is a misnomer since this method does not actually wrap the
+// given value. Though unlikely, the operation can technically fail,
+// so the recommended function allows the return of an error instead
+// of panic'ing.
+func WrapEnum(ed protoreflect.EnumDescriptor) protoreflect.EnumDescriptor {
+	result, err := AddSourceInfoToEnum(ed)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+// WrapService is present for backwards-compatibility reasons. It calls
+// AddSourceInfoToService and panics if that function returns an error.
+//
+// Deprecated: Use AddSourceInfoToService directly instead. The word
+// "wrap" is a misnomer since this method does not actually wrap the
+// given value. Though unlikely, the operation can technically fail,
+// so the recommended function allows the return of an error instead
+// of panic'ing.
+func WrapService(sd protoreflect.ServiceDescriptor) protoreflect.ServiceDescriptor {
+	result, err := AddSourceInfoToService(sd)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+// WrapExtensionType is present for backwards-compatibility reasons. It
+// calls AddSourceInfoToExtensionType and panics if that function
+// returns an error.
+//
+// Deprecated: Use AddSourceInfoToExtensionType directly instead. The
+// word "wrap" is a misnomer since this method does not actually wrap
+// the given value. Though unlikely, the operation can technically fail,
+// so the recommended function allows the return of an error instead
+// of panic'ing.
+func WrapExtensionType(xt protoreflect.ExtensionType) protoreflect.ExtensionType {
+	result, err := AddSourceInfoToExtensionType(xt)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+// WrapMessageType is present for backwards-compatibility reasons. It
+// calls AddSourceInfoToMessageType and panics if that function returns
+// an error.
+//
+// Deprecated: Use AddSourceInfoToMessageType directly instead. The word
+// "wrap" is a misnomer since this method does not actually wrap the
+// given value. Though unlikely, the operation can technically fail, so
+// the recommended function allows the return of an error instead of
+// panic'ing.
+func WrapMessageType(mt protoreflect.MessageType) protoreflect.MessageType {
+	result, err := AddSourceInfoToMessageType(mt)
+	if err != nil {
+		panic(err)
+	}
+	return result
 }
 
 type extensionType struct {
