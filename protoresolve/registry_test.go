09110551087f9d7bf7a6167089ebe56567609a8d
Aaditya-ops-cpu <karthaaaditya@gmail.com>
2026-01-13 16:58:45 +0530

    test(protoresolve): implement missing logic in testResolver helper

--- protoresolve/registry_test.go
+++ protoresolve/registry_test.go
@@ -6,21 +6,35 @@ import (
 	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/reflect/protoregistry"
 
+	"github.com/jhump/protoreflect/v2/internal/testprotos"
 	"github.com/jhump/protoreflect/v2/protoresolve"
 )
 
 func TestRegistry(t *testing.T) {
-	// TODO
-	testResolver(t, &protoresolve.Registry{})
+	reg := &protoresolve.Registry{}
+	// Register the file locally (safe, as 'reg' is a new variable)
+	require.NoError(t, reg.RegisterFile(testprotos.File_desc_test1_proto))
+
+	testResolver(t, reg)
 }
 
-func TestFromFiles(t *testing.T) {
-	// TODO
+func TestFromFilaes(t *testing.T) {
+	// 1. Setup GlobalFiles (SAFE VERSION)
+	// We check if the file is already there. If NOT found, we register it.
+	// This prevents the "already registered" panic.
+	if _, err := protoregistry.GlobalFiles.FindFileByPath("desc_test1.proto"); err != nil {
+		_ = protoregistry.GlobalFiles.RegisterFile(testprotos.File_desc_test1_proto)
+	}
+
 	reg, err := protoresolve.FromFiles(protoregistry.GlobalFiles)
 	require.NoError(t, err)
 	testResolver(t, reg)
 
-	reg, err = protoresolve.FromFiles(&protoregistry.Files{})
+	// 2. Setup local Files
+	var files protoregistry.Files
+	require.NoError(t, files.RegisterFile(testprotos.File_desc_test1_proto))
+
+	reg, err = protoresolve.FromFiles(&files)
 	require.NoError(t, err)
 	testResolver(t, reg)
 }
