47a1755e4cbd0f551c5cd028abbfc883b7d6e85b
Aaditya-ops-cpu <karthaaaditya@gmail.com>
2026-01-13 17:28:10 +0530

    rename TestFromFilaes to TestFromFiles

--- protoresolve/registry_test.go
+++ protoresolve/registry_test.go
@@ -12,16 +12,14 @@ import (
 
 func TestRegistry(t *testing.T) {
 	reg := &protoresolve.Registry{}
-	// Register the file locally (safe, as 'reg' is a new variable)
+	// Register the file locally (safe, as 'reg' is a new variable introduced here)
 	require.NoError(t, reg.RegisterFile(testprotos.File_desc_test1_proto))
 
 	testResolver(t, reg)
 }
 
-func TestFromFilaes(t *testing.T) {
-	// 1. Setup GlobalFiles (SAFE VERSION)
-	// We check if the file is already there. If NOT found, we register it.
-	// This prevents the "already registered" panic.
+func TestFromFiles(t *testing.T) {
+	// Setting up GlobalFiles .We check if the file is already there. If not found, we register it.
 	if _, err := protoregistry.GlobalFiles.FindFileByPath("desc_test1.proto"); err != nil {
 		_ = protoregistry.GlobalFiles.RegisterFile(testprotos.File_desc_test1_proto)
 	}
@@ -30,7 +28,7 @@ func TestFromFilaes(t *testing.T) {
 	require.NoError(t, err)
 	testResolver(t, reg)
 
-	// 2. Setup local Files
+	// Setting up local Files
 	var files protoregistry.Files
 	require.NoError(t, files.RegisterFile(testprotos.File_desc_test1_proto))
 
